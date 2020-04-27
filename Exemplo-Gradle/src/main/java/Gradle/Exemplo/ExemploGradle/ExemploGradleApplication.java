e10de698a90f3fdb39fa37ded552e5f87112c2d1
Willian Lopes da Silva <64399271+Willian02@users.noreply.github.com>
2020-04-27 06:48:18 -0300

    Aula de Spring

---
+++ Exemplo-Gradle/src/main/java/Gradle/Exemplo/ExemploGradle/ExemploGradleApplication.java
@@ -0,0 +1,16 @@
+package Gradle.Exemplo.ExemploGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGradleApplication.class, args);
+
+		System.out.println("Exemplo Gradle");
+
+	}
+
+}
---
+++ Exemplo-Gradle/src/test/java/Gradle/Exemplo/ExemploGradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package Gradle.Exemplo.ExemploGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
