8a23866e1165b1882cbe7e3a52a9d146d1c8400a
Willian Lopes da Silva <64399271+Willian02@users.noreply.github.com>
2020-04-27 06:40:33 -0300

    Aula de Spring

---
+++ Exemplo-Maven/src/main/java/Maven/Exemplo/ExemploMaven/ExemploMavenApplication.java
@@ -0,0 +1,16 @@
+package Maven.Exemplo.ExemploMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMavenApplication.class, args);
+
+		System.out.println("Exemplo Maven");
+
+	}
+
+}
