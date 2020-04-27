598bc6ab9b5d87129d237343e5ab314c16008fc0
Willian Lopes da Silva <64399271+Willian02@users.noreply.github.com>
2020-04-27 07:00:48 -0300

    Aula de Spring

---
+++ Exemplo-GradleWeb/src/main/java/GradleWeb/Exemplo/ExemploGradleWeb/ExemploGradleWebApplication.java
@@ -0,0 +1,16 @@
+package GradleWeb.Exemplo.ExemploGradleWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGradleWebApplication.class, args);
+
+		System.out.println("Exemplo com server TonCat");
+
+	}
+
+}
---
+++ Exemplo-GradleWeb/src/test/java/GradleWeb/Exemplo/ExemploGradleWeb/ExemploGradleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package GradleWeb.Exemplo.ExemploGradleWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
